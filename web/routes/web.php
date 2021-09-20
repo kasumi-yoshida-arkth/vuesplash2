097de560dd52dbbde680be47445c98a42bf7a744
su-min484 <85155408+su-min484@users.noreply.github.com>
2021-09-20 23:06:01 +0900

    install vue-router

--- web/routes/web.php
+++ web/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 例えば/signupというURLへのアクセスでも以下の場合はindexのテンプレートが返却される。
+// 画面遷移はフロントエンドのVueRouterが制御する
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -17,11 +17,8 @@ const mix = require('laravel-mix');
 //     ]);
 
 // browserSyncはJSやPHPファイルが変更された時に自動的にブラウザがリロードされる
-mix.browserSync({
-    proxy: '0.0.0.0:8081', // アプリの起動アドレス
-    open: false // ブラウザを自動で開かない
-})
+mix.browserSync('vuesplash.test')
 //  JSとVueコンポーネントをコンパイルする
-  .js('resources/js/app.js', 'public/js')
+  .js('resources/js/app.js', 'public/js').vue()
 //  コンパイルしたファイルのバージョニングが有効になる
   .version()
