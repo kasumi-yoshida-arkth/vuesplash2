27ba4ec93591509370ec15af53a1b81fb0bf0010
kasumi_yoshida <k.yoshida@arkth.co.jp>
2021-09-27 23:10:21 +0900

    CSRF対策

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -6,12 +6,18 @@ import Vue from 'vue'
 
 // ルーティングの定義をインポートする
 import router from './router'
+
+import store from './store'
+
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+
+import './bootstrap'
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store, // Vuexのファイル
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
